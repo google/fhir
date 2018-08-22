5ddbe6ad7eb53d17e6b5a2dea6775d8597684b9f
Patrik Sundberg <sundberg@google.com>
2018-08-21 20:59:43 -0400

    Add a short list of constants for commonly used feature names.
    PiperOrigin-RevId: 209693413

---
+++ cc/google/fhir/seqex/feature_keys.cc
@@ -0,0 +1,30 @@
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "google/fhir/seqex/feature_keys.h"
+
+namespace google {
+namespace fhir {
+namespace seqex {
+
+const char kEncounterIdFeatureKey[] = "encounterId";
+const char kEventIdFeatureKey[] = "eventId";
+const char kLabelEncounterIdFeatureKey[] = "currentEncounterId";
+const char kLabelTimestampFeatureKey[] = "timestamp";
+const char kPatientIdFeatureKey[] = "patientId";
+const char kResourceIdFeatureKey[] = "resourceId";
+
+}  // namespace seqex
+}  // namespace fhir
+}  // namespace google
---
+++ cc/google/fhir/seqex/feature_keys.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef GOOGLE_FHIR_SEQEX_FEATURE_KEYS_H_
+#define GOOGLE_FHIR_SEQEX_FEATURE_KEYS_H_
+
+namespace google {
+namespace fhir {
+namespace seqex {
+
+extern const char kEncounterIdFeatureKey[];
+extern const char kEventIdFeatureKey[];
+extern const char kLabelEncounterIdFeatureKey[];
+extern const char kLabelTimestampFeatureKey[];
+extern const char kPatientIdFeatureKey[];
+extern const char kResourceIdFeatureKey[];
+
+}  // namespace seqex
+}  // namespace fhir
+}  // namespace google
+
+#endif  // GOOGLE_FHIR_SEQEX_FEATURE_KEYS_H_
