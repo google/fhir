25d8c471ee68e46b00a8d58d8d579049e9657406
Patrik Sundberg <sundberg@google.com>
2018-08-22 19:23:36 -0400

    Add more tests for ResourceToExample.
    PiperOrigin-RevId: 209849778

--- cc/google/fhir/seqex/resource_to_example_test.cc
+++ cc/google/fhir/seqex/resource_to_example_test.cc
@@ -12,9 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 #include <memory>
 
+#include "gflags/gflags.h"
 #include "google/protobuf/text_format.h"
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
@@ -25,6 +25,8 @@
 #include "tensorflow/core/example/example.pb.h"
 #include "tensorflow/core/example/feature.pb.h"
 #include "tensorflow/core/platform/env.h"
+
+DECLARE_bool(tokenize_code_text_features);
 
 namespace google {
 namespace fhir {
@@ -40,7 +42,7 @@ class ResourceToExampleTest : public ::testing::Test {
   google::protobuf::TextFormat::Parser parser_;
 };
 
-TEST_F(ResourceToExampleTest, ResourceToExample_Patient) {
+TEST_F(ResourceToExampleTest, Patient) {
   stu3::proto::Patient patient = PARSE_VALID_STU3_PROTO(R"(
     id { value: "1" }
     gender { value: FEMALE }
@@ -74,10 +76,138 @@ TEST_F(ResourceToExampleTest, ResourceToExample_Patient) {
 
   ::tensorflow::Example output;
   ResourceToExample(patient, &output, false);
-  EXPECT_THAT(expected, EqualsProto(output));
+  EXPECT_THAT(output, EqualsProto(expected));
 }
 
-TEST_F(ResourceToExampleTest, ResourceToExample_PositiveInt) {
+TEST_F(ResourceToExampleTest, Observation_ValueQuantity) {
+  FLAGS_tokenize_code_text_features = true;
+  stu3::proto::Observation input;
+  ASSERT_TRUE(parser_.ParseFromString(R"proto(
+    id { value: "123" }
+    subject: { patient_id { value: "456" } }
+    code: {
+      coding: {
+        system { value: "http://loinc.org" }
+        code { value: "LAB50" }
+      }
+      text { value: "BILIRUBIN, TOTAL" }
+    }
+    effective {
+      date_time {
+        value_us: 2167084800000000  # "2038-09-02T20:00:00-04:00"
+        precision: DAY
+        timezone: "America/New_York"
+      }
+    }
+    value {
+      quantity {
+        value { value: "0.5" }
+        unit { value: "mEq/L" }
+      }
+    }
+  )proto", &input));
+  ::tensorflow::Example expected;
+  ASSERT_TRUE(parser_.ParseFromString(R"proto(
+    features {
+      feature {
+        key: "Observation.code"
+        value { bytes_list { value: "loinc:LAB50" } }
+      }
+      feature {
+        key: "Observation.code.loinc"
+        value { bytes_list { value: "LAB50" } }
+      }
+      feature {
+        key: "Observation.code.text.tokenized"
+        value { bytes_list { value: "bilirubin" value: "total" } }
+      }
+      feature {
+        key: "Observation.effective.dateTime"
+        value { int64_list { value: 2167084800 } }
+      }
+      feature {
+        key: "Observation.value.quantity.value"
+        value { float_list { value: 0.5 } }
+      }
+      feature {
+        key: "Observation.value.quantity.unit"
+        value { bytes_list { value: "mEq/L" } }
+      }
+    }
+  )proto", &expected));
+
+  ::tensorflow::Example output;
+  ResourceToExample(input, &output, false);
+  EXPECT_THAT(output, EqualsProto(expected));
+}
+
+TEST_F(ResourceToExampleTest, Observation_TwoCodes) {
+  FLAGS_tokenize_code_text_features = false;
+  stu3::proto::Observation input;
+  ASSERT_TRUE(parser_.ParseFromString(R"proto(
+    id { value: "123" }
+    subject: { patient_id { value: "456" } }
+    code: {
+      coding: {
+        system { value: "http://hl7.org/fhir/sid/icd-9-cm" }
+        code { value: "bilirubin" }
+        display { value: "total bilirubin" }
+      }
+      coding: {
+        system { value: "http://loinc.org" }
+        code { value: "LAB50" }
+      }
+      text { value: "BILIRUBIN, TOTAL" }
+    }
+    effective {
+      date_time {
+        value_us: 2167084800000000  # "2038-09-02T20:00:00-04:00"
+        precision: DAY
+        timezone: "America/New_York"
+      }
+    }
+    value { string_value { value: "freeform" } }
+  )proto", &input));
+  ::tensorflow::Example expected;
+  ASSERT_TRUE(parser_.ParseFromString(R"proto(
+    features {
+      feature {
+        key: "Observation.code"
+        value { bytes_list { value: "loinc:LAB50" } }
+      }
+      feature {
+        key: "Observation.code.icd9"
+        value { bytes_list { value: "bilirubin" } }
+      }
+      feature {
+        key: "Observation.code.icd9.display"
+        value { bytes_list { value: "total bilirubin" } }
+      }
+      feature {
+        key: "Observation.code.loinc"
+        value { bytes_list { value: "LAB50" } }
+      }
+      feature {
+        key: "Observation.code.text"
+        value { bytes_list { value: "BILIRUBIN, TOTAL" } }
+      }
+      feature {
+        key: "Observation.effective.dateTime"
+        value { int64_list { value: 2167084800 } }
+      }
+      feature {
+        key: "Observation.value.string"
+        value { bytes_list { value: "freeform" } }
+      }
+    }
+  )proto", &expected));
+
+  ::tensorflow::Example output;
+  ResourceToExample(input, &output, false);
+  EXPECT_THAT(output, EqualsProto(expected));
+}
+
+TEST_F(ResourceToExampleTest, PositiveInt) {
   stu3::proto::Encounter input;
   ASSERT_TRUE(parser_.ParseFromString(R"proto(
     diagnosis {
@@ -109,7 +239,7 @@ TEST_F(ResourceToExampleTest, ResourceToExample_PositiveInt) {
     })proto", &expected));
   ::tensorflow::Example output;
   ResourceToExample(input, &output, false);
-  EXPECT_THAT(expected, EqualsProto(output));
+  EXPECT_THAT(output, EqualsProto(expected));
 }
 
 }  // namespace seqex
