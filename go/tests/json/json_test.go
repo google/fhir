e2571c05f4e0561d2063ddca00e93ade3cc594d0
Arran Schlosberg <me@arranschlosberg.com>
2018-11-18 16:13:11 +0000

    JSON marshalling + unmarshalling of all integer types + Date

--- go/tests/json/json_test.go
+++ go/tests/json/json_test.go
@@ -51,16 +51,25 @@ func TestGoodConversions(t *testing.T) {
 			json: `false`,
 		},
 		{
-			msg: &pb.String{
-				Value: `hello world`,
+			msg: &pb.Date{
+				ValueUs:   504921600000000,
+				Precision: pb.Date_YEAR,
 			},
-			json: `"hello world"`,
+			json: `"1986"`,
 		},
 		{
-			msg: &pb.String{
-				Value: `"double" 'single'`,
+			msg: &pb.Date{
+				ValueUs:   528508800000000,
+				Precision: pb.Date_MONTH,
 			},
-			json: `"\"double\" 'single'"`,
+			json: `"1986-10"`,
+		},
+		{
+			msg: &pb.Date{
+				ValueUs:   529977600000000,
+				Precision: pb.Date_DAY,
+			},
+			json: `"1986-10-18"`,
 		},
 		{
 			msg: &pb.Integer{
@@ -79,6 +88,66 @@ func TestGoodConversions(t *testing.T) {
 				Value: -42,
 			},
 			json: `-42`,
+		},
+		{
+			msg: &pb.Integer{
+				Value: math.MaxInt32,
+			},
+			json: `2147483647`,
+		},
+		{
+			msg: &pb.Integer{
+				Value: math.MinInt32,
+			},
+			json: `-2147483648`,
+		},
+		{
+			msg: &pb.PositiveInt{
+				Value: 1,
+			},
+			json: `1`,
+		},
+		{
+			msg: &pb.PositiveInt{
+				Value: 42,
+			},
+			json: `42`,
+		},
+		{
+			msg: &pb.PositiveInt{
+				Value: math.MaxUint32,
+			},
+			json: `4294967295`,
+		},
+		{
+			msg: &pb.String{
+				Value: `hello world`,
+			},
+			json: `"hello world"`,
+		},
+		{
+			msg: &pb.String{
+				Value: `"double" 'single'`,
+			},
+			json: `"\"double\" 'single'"`,
+		},
+		{
+			msg: &pb.UnsignedInt{
+				Value: 0,
+			},
+			json: `0`,
+		},
+		{
+			msg: &pb.UnsignedInt{
+				Value: 42,
+			},
+			json: `42`,
+		},
+		{
+			msg: &pb.UnsignedInt{
+				Value: math.MaxUint32,
+			},
+			json: `4294967295`,
 		},
 	}
 
@@ -118,13 +187,54 @@ func TestBadJSON(t *testing.T) {
 		wantErrContains string
 	}{
 		{
+			msg:             &pb.Date{},
+			json:            `"1986-"`,
+			wantErrContains: "regex",
+		},
+		{
 			msg:             &pb.Integer{},
 			json:            `"x"`,
 			wantErrContains: "regex",
 		},
 		{
 			msg:             &pb.Integer{},
-			json:            fmt.Sprintf("%d", math.MaxInt64), // only supports 32-bit
+			json:            fmt.Sprintf("%d", int64(math.MaxInt32)+1),
+			wantErrContains: "32",
+		},
+		{
+			msg:             &pb.Integer{},
+			json:            fmt.Sprintf("%d", int64(math.MinInt32)-1),
+			wantErrContains: "32",
+		},
+		{
+			msg:             &pb.PositiveInt{},
+			json:            `"x"`,
+			wantErrContains: "regex",
+		},
+		{
+			msg:  &pb.PositiveInt{},
+			json: `0`,
+			// there is also an explicit test, but the regex catches it first
+			wantErrContains: "regex",
+		},
+		{
+			msg:             &pb.PositiveInt{},
+			json:            fmt.Sprintf("%d", uint64(math.MaxUint32)+1),
+			wantErrContains: "32",
+		},
+		{
+			msg:             &pb.String{},
+			json:            `""`,
+			wantErrContains: "empty",
+		},
+		{
+			msg:             &pb.UnsignedInt{},
+			json:            `"x"`,
+			wantErrContains: "regex",
+		},
+		{
+			msg:             &pb.UnsignedInt{},
+			json:            fmt.Sprintf("%d", uint64(math.MaxUint32)+1),
 			wantErrContains: "32",
 		},
 	}
@@ -137,8 +247,42 @@ func TestBadJSON(t *testing.T) {
 			if err := jsonpb.UnmarshalString(json, got); err == nil {
 				t.Fatalf("UnmarshalString(%s, %T) got nil err; want err", json, got)
 			} else if !strings.Contains(err.Error(), tt.wantErrContains) {
-				t.Fatalf("UnmarshalString(%s, %T) got err %v; want containing", json, got, err, tt.wantErrContains)
+				t.Fatalf("UnmarshalString(%s, %T) got err %v; want containing %q", json, got, err, tt.wantErrContains)
 			}
 		})
 	}
 }
+
+func TestBadProto(t *testing.T) {
+	tests := []struct {
+		// msg is used merely to define the type to which the JSON should be
+		msg             proto.Message
+		wantErrContains string
+	}{
+		{
+			msg: &pb.Date{
+				Timezone: "+0100",
+			},
+			wantErrContains: "zone",
+		},
+		{
+			msg: &pb.String{
+				Value: "",
+			},
+			wantErrContains: "empty",
+		},
+	}
+
+	for _, tt := range tests {
+		msg := tt.msg
+		t.Run(fmt.Sprintf("%T", msg), func(t *testing.T) {
+			if _, err := (&jsonpb.Marshaler{}).MarshalToString(msg); err == nil {
+				t.Fatalf("MarshalToString(%T(%+v)) got nil err; want err", msg, msg)
+			} else if !strings.Contains(err.Error(), tt.wantErrContains) {
+				t.Fatalf("MarshalToString(%T(%+v)) got err %v; want containing %q", msg, msg, err, tt.wantErrContains)
+			}
+		})
+	}
+}
+
+// TODO(arrans) test pb.Date.Time() rounding
--- proto/stu3/json.go
+++ proto/stu3/json.go
@@ -19,11 +19,14 @@
 package stu3
 
 import (
+	"bytes"
 	"encoding/base64"
+	"errors"
 	"fmt"
 	"regexp"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/golang/protobuf/descriptor"
 	"github.com/golang/protobuf/jsonpb"
@@ -36,7 +39,7 @@ const (
 )
 
 // jsonString returns s as a JSON string in double quotes, escaping any existing
-// quotes.
+// double quotes.
 func jsonString(s string) []byte {
 	return []byte(fmt.Sprintf(
 		`"%s"`,
@@ -44,20 +47,29 @@ func jsonString(s string) []byte {
 	))
 }
 
-// parseJSONString strips surrounding double quotes and unescapes any remaining
-// quotes.
-func parseJSONString(buf []byte) (string, error) {
-	n := len(buf)
-	if n < 2 || buf[0] != '"' || buf[n-1] != '"' {
-		return "", fmt.Errorf("invalid JSON string %s", buf)
+// reMatchString strips surrounding double quotes and unescapes remaining double
+// quotes, i.e. undoes jsonString(), before checking the resulting string with
+// reMatch() and returning it.
+func reMatchString(msg descriptor.Message, json []byte) (string, error) {
+	n := len(json)
+	if n < 2 || json[0] != '"' || json[n-1] != '"' {
+		return "", fmt.Errorf("invalid JSON string %s", json)
 	}
-	s := string(buf[1 : n-1])
-	return strings.Replace(s, escapedDoubleQuote, doubleQuote, -1), nil
+	json = bytes.Replace(
+		json[1:n-1],
+		[]byte(escapedDoubleQuote), []byte(doubleQuote), -1,
+	)
+	if err := reMatch(msg, json); err != nil {
+		return "", err
+	}
+	return string(json), nil
 }
 
-// reMatch checks if the JSON string matches the regular expression in the
-// proto option "value_regex".
+// reMatch checks if the byte slices matches the regular expression in the proto
+// option "value_regex". If json contains a JSON string, it must already parsed;
+// see reMatchString().
 func reMatch(msg descriptor.Message, json []byte) error {
+	// TODO(arrans) memoise and benchmark extraction and compilation of regexes.
 	_, md := descriptor.ForMessage(msg)
 	if !proto.HasExtension(md.Options, E_ValueRegex) {
 		return nil
@@ -72,9 +84,9 @@ func reMatch(msg descriptor.Message, json []byte) error {
 		if s == nil {
 			return nil
 		}
-		// The FHIR definition states that "regexes should be qualified with start
-		// of string and end of string anchors based on the regex implementation
-		// used" - http://hl7.org/fhir/datatypes.html
+		// The FHIR definition states that "regexes should be qualified with
+		// start of string and end of string anchors based on the regex
+		// implementation used" - http://hl7.org/fhir/datatypes.html
 		reStr := fmt.Sprintf("^%s$", *s)
 
 		re, err := regexp.Compile(reStr)
@@ -89,23 +101,20 @@ func reMatch(msg descriptor.Message, json []byte) error {
 		}
 		return nil
 	default:
-		return fmt.Errorf("value_regex for %T option of type %T", msg, s)
+		return fmt.Errorf("value_regex for %T option of type %T; expecting string", msg, s)
 	}
 }
 
 // MarshalJSONPB implements the single method of the jsonpb.JSONPBMarshaler
-// interface.
+// interface, to properly handle this FHIR primitive type.
 func (p *Base64Binary) MarshalJSONPB(_ *jsonpb.Marshaler) ([]byte, error) {
 	return jsonString(base64.StdEncoding.EncodeToString(p.Value)), nil
 }
 
 // UnmarshalJSONPB implements the single method of the jsonpb.JSONPBUnmarshaler
-// interface.
+// interface, to properly handle this FHIR primitive type.
 func (p *Base64Binary) UnmarshalJSONPB(_ *jsonpb.Unmarshaler, buf []byte) error {
-	if err := reMatch(p, buf); err != nil {
-		return err
-	}
-	s, err := parseJSONString(buf)
+	s, err := reMatchString(p, buf)
 	if err != nil {
 		return err
 	}
@@ -118,7 +127,7 @@ func (p *Base64Binary) UnmarshalJSONPB(_ *jsonpb.Unmarshaler, buf []byte) error 
 }
 
 // MarshalJSONPB implements the single method of the jsonpb.JSONPBMarshaler
-// interface.
+// interface, to properly handle this FHIR primitive type.
 func (p *Boolean) MarshalJSONPB(_ *jsonpb.Marshaler) ([]byte, error) {
 	if p.Value {
 		return []byte(`true`), nil
@@ -127,7 +136,7 @@ func (p *Boolean) MarshalJSONPB(_ *jsonpb.Marshaler) ([]byte, error) {
 }
 
 // UnmarshalJSONPB implements the single method of the jsonpb.JSONPBUnmarshaler
-// interface.
+// interface, to properly handle this FHIR primitive type.
 func (p *Boolean) UnmarshalJSONPB(_ *jsonpb.Unmarshaler, buf []byte) error {
 	if err := reMatch(p, buf); err != nil {
 		return err
@@ -144,13 +153,13 @@ func (p *Boolean) UnmarshalJSONPB(_ *jsonpb.Unmarshaler, buf []byte) error {
 }
 
 // MarshalJSONPB implements the single method of the jsonpb.JSONPBMarshaler
-// interface.
+// interface, to properly handle this FHIR primitive type.
 func (p *Code) MarshalJSONPB(_ *jsonpb.Marshaler) ([]byte, error) {
 	return nil, fmt.Errorf("MarshalJSONPB unimplemented for %T", p)
 }
 
 // UnmarshalJSONPB implements the single method of the jsonpb.JSONPBUnmarshaler
-// interface.
+// interface, to properly handle this FHIR primitive type.
 func (p *Code) UnmarshalJSONPB(_ *jsonpb.Unmarshaler, buf []byte) error {
 	if err := reMatch(p, buf); err != nil {
 		return err
@@ -158,29 +167,110 @@ func (p *Code) UnmarshalJSONPB(_ *jsonpb.Unmarshaler, buf []byte) error {
 	return fmt.Errorf("UnmarshalJSONPB unimplemented for %T", p)
 }
 
+const (
+	dateLayoutYear  = `2006`
+	dateLayoutMonth = `2006-01`
+	dateLayoutDay   = `2006-01-02`
+
+	// Go layout format for convenience rather than checking the docs:
+	// Mon Jan 2 15:04:05 -0700 MST 2006
+)
+
+const (
+	unitToMicro = int64(time.Second / time.Microsecond)
+	microToNano = int64(time.Microsecond / time.Nanosecond)
+)
+
+// utcFromMicroSec returns a time in UTC from unix microseconds.
+func utcFromMicroSec(usec int64) time.Time {
+	return time.Unix(usec/unitToMicro, (usec%microToNano)*microToNano).UTC()
+}
+
 // MarshalJSONPB implements the single method of the jsonpb.JSONPBMarshaler
-// interface.
+// interface, to properly handle this FHIR primitive type.
 func (p *Date) MarshalJSONPB(_ *jsonpb.Marshaler) ([]byte, error) {
-	return nil, fmt.Errorf("MarshalJSONPB unimplemented for %T", p)
+	if p.Timezone != "" {
+		return nil, fmt.Errorf("FHIR date primitive SHALL not have timezone; has %q", p.Timezone)
+	}
+	var layout string
+	switch p.Precision {
+	case Date_YEAR:
+		layout = dateLayoutYear
+	case Date_MONTH:
+		layout = dateLayoutMonth
+	case Date_DAY:
+		layout = dateLayoutDay
+	default:
+		return nil, fmt.Errorf("unknown precision[%d]", p.Precision)
+	}
+	return jsonString(p.Time().Format(layout)), nil
+}
+
+var datePrecisionLayout = map[Date_Precision]string{
+	Date_YEAR:  dateLayoutYear,
+	Date_MONTH: dateLayoutMonth,
+	Date_DAY:   dateLayoutDay,
 }
 
 // UnmarshalJSONPB implements the single method of the jsonpb.JSONPBUnmarshaler
-// interface.
+// interface, to properly handle this FHIR primitive type.
 func (p *Date) UnmarshalJSONPB(_ *jsonpb.Unmarshaler, buf []byte) error {
-	if err := reMatch(p, buf); err != nil {
+	s, err := reMatchString(p, buf)
+	if err != nil {
 		return err
 	}
-	return fmt.Errorf("UnmarshalJSONPB unimplemented for %T", p)
+	for prec, layout := range datePrecisionLayout {
+		t, err := time.ParseInLocation(layout, s, time.UTC)
+		if err != nil {
+			continue
+		}
+		p.ValueUs = t.UnixNano() / microToNano
+		p.Precision = prec
+		return nil
+	}
+	return fmt.Errorf("invalid date %s", buf)
 }
 
+// Time returns the Date object as a UTC Time, rounded based on Precision.
+// Rounding is not performed with time.Time.Round() but rather by setting all
+// finer-grain time scales to "zero";" i.e. YEAR-precision will always be
+// January 1st at 00:00, MONTH-precision will always be 1st of the month at
+// 00:00, and DAY will always be at 00:00 time.
+func (p *Date) Time() time.Time {
+	t := utcFromMicroSec(p.ValueUs)
+
+	// 00:00 time
+	sub := time.Duration(t.Hour())*time.Hour +
+		time.Duration(t.Minute())*time.Minute +
+		time.Duration(t.Second())*time.Second
+	t = t.Add(-sub)
+
+	var subMonths, subDays int
+	switch p.Precision {
+	case Date_YEAR:
+		subMonths = int(t.Month() - time.January)
+		fallthrough
+	case Date_MONTH:
+		subDays = t.Day() - 1
+	}
+
+	return t.AddDate(0, -subMonths, -subDays)
+}
+
+// TODO(arrans): implement AsDate()
+// AsDate returns a Date at the specified time, after rounding according to the
+// specified precision.
+//func AsDate(t time.Time, p Date_Precision) *Date {
+//}
+
 // MarshalJSONPB implements the single method of the jsonpb.JSONPBMarshaler
-// interface.
+// interface, to properly handle this FHIR primitive type.
 func (p *DateTime) MarshalJSONPB(_ *jsonpb.Marshaler) ([]byte, error) {
 	return nil, fmt.Errorf("MarshalJSONPB unimplemented for %T", p)
 }
 
 // UnmarshalJSONPB implements the single method of the jsonpb.JSONPBUnmarshaler
-// interface.
+// interface, to properly handle this FHIR primitive type.
 func (p *DateTime) UnmarshalJSONPB(_ *jsonpb.Unmarshaler, buf []byte) error {
 	if err := reMatch(p, buf); err != nil {
 		return err
@@ -189,13 +279,13 @@ func (p *DateTime) UnmarshalJSONPB(_ *jsonpb.Unmarshaler, buf []byte) error {
 }
 
 // MarshalJSONPB implements the single method of the jsonpb.JSONPBMarshaler
-// interface.
+// interface, to properly handle this FHIR primitive type.
 func (p *Decimal) MarshalJSONPB(_ *jsonpb.Marshaler) ([]byte, error) {
 	return nil, fmt.Errorf("MarshalJSONPB unimplemented for %T", p)
 }
 
 // UnmarshalJSONPB implements the single method of the jsonpb.JSONPBUnmarshaler
-// interface.
+// interface, to properly handle this FHIR primitive type.
 func (p *Decimal) UnmarshalJSONPB(_ *jsonpb.Unmarshaler, buf []byte) error {
 	if err := reMatch(p, buf); err != nil {
 		return err
@@ -204,13 +294,13 @@ func (p *Decimal) UnmarshalJSONPB(_ *jsonpb.Unmarshaler, buf []byte) error {
 }
 
 // MarshalJSONPB implements the single method of the jsonpb.JSONPBMarshaler
-// interface.
+// interface, to properly handle this FHIR primitive type.
 func (p *Id) MarshalJSONPB(_ *jsonpb.Marshaler) ([]byte, error) {
 	return nil, fmt.Errorf("MarshalJSONPB unimplemented for %T", p)
 }
 
 // UnmarshalJSONPB implements the single method of the jsonpb.JSONPBUnmarshaler
-// interface.
+// interface, to properly handle this FHIR primitive type.
 func (p *Id) UnmarshalJSONPB(_ *jsonpb.Unmarshaler, buf []byte) error {
 	if err := reMatch(p, buf); err != nil {
 		return err
@@ -219,13 +309,13 @@ func (p *Id) UnmarshalJSONPB(_ *jsonpb.Unmarshaler, buf []byte) error {
 }
 
 // MarshalJSONPB implements the single method of the jsonpb.JSONPBMarshaler
-// interface.
+// interface, to properly handle this FHIR primitive type.
 func (p *Instant) MarshalJSONPB(_ *jsonpb.Marshaler) ([]byte, error) {
 	return nil, fmt.Errorf("MarshalJSONPB unimplemented for %T", p)
 }
 
 // UnmarshalJSONPB implements the single method of the jsonpb.JSONPBUnmarshaler
-// interface.
+// interface, to properly handle this FHIR primitive type.
 func (p *Instant) UnmarshalJSONPB(_ *jsonpb.Unmarshaler, buf []byte) error {
 	if err := reMatch(p, buf); err != nil {
 		return err
@@ -234,13 +324,13 @@ func (p *Instant) UnmarshalJSONPB(_ *jsonpb.Unmarshaler, buf []byte) error {
 }
 
 // MarshalJSONPB implements the single method of the jsonpb.JSONPBMarshaler
-// interface.
+// interface, to properly handle this FHIR primitive type.
 func (p *Integer) MarshalJSONPB(_ *jsonpb.Marshaler) ([]byte, error) {
 	return []byte(fmt.Sprintf("%d", p.Value)), nil
 }
 
 // UnmarshalJSONPB implements the single method of the jsonpb.JSONPBUnmarshaler
-// interface.
+// interface, to properly handle this FHIR primitive type.
 func (p *Integer) UnmarshalJSONPB(_ *jsonpb.Unmarshaler, buf []byte) error {
 	if err := reMatch(p, buf); err != nil {
 		return err
@@ -254,13 +344,13 @@ func (p *Integer) UnmarshalJSONPB(_ *jsonpb.Unmarshaler, buf []byte) error {
 }
 
 // MarshalJSONPB implements the single method of the jsonpb.JSONPBMarshaler
-// interface.
+// interface, to properly handle this FHIR primitive type.
 func (p *Markdown) MarshalJSONPB(_ *jsonpb.Marshaler) ([]byte, error) {
 	return nil, fmt.Errorf("MarshalJSONPB unimplemented for %T", p)
 }
 
 // UnmarshalJSONPB implements the single method of the jsonpb.JSONPBUnmarshaler
-// interface.
+// interface, to properly handle this FHIR primitive type.
 func (p *Markdown) UnmarshalJSONPB(_ *jsonpb.Unmarshaler, buf []byte) error {
 	if err := reMatch(p, buf); err != nil {
 		return err
@@ -269,13 +359,13 @@ func (p *Markdown) UnmarshalJSONPB(_ *jsonpb.Unmarshaler, buf []byte) error {
 }
 
 // MarshalJSONPB implements the single method of the jsonpb.JSONPBMarshaler
-// interface.
+// interface, to properly handle this FHIR primitive type.
 func (p *Oid) MarshalJSONPB(_ *jsonpb.Marshaler) ([]byte, error) {
 	return nil, fmt.Errorf("MarshalJSONPB unimplemented for %T", p)
 }
 
 // UnmarshalJSONPB implements the single method of the jsonpb.JSONPBUnmarshaler
-// interface.
+// interface, to properly handle this FHIR primitive type.
 func (p *Oid) UnmarshalJSONPB(_ *jsonpb.Unmarshaler, buf []byte) error {
 	if err := reMatch(p, buf); err != nil {
 		return err
@@ -284,48 +374,61 @@ func (p *Oid) UnmarshalJSONPB(_ *jsonpb.Unmarshaler, buf []byte) error {
 }
 
 // MarshalJSONPB implements the single method of the jsonpb.JSONPBMarshaler
-// interface.
+// interface, to properly handle this FHIR primitive type.
 func (p *PositiveInt) MarshalJSONPB(_ *jsonpb.Marshaler) ([]byte, error) {
-	return nil, fmt.Errorf("MarshalJSONPB unimplemented for %T", p)
+	return []byte(fmt.Sprintf("%d", p.Value)), nil
 }
 
 // UnmarshalJSONPB implements the single method of the jsonpb.JSONPBUnmarshaler
-// interface.
+// interface, to properly handle this FHIR primitive type.
 func (p *PositiveInt) UnmarshalJSONPB(_ *jsonpb.Unmarshaler, buf []byte) error {
 	if err := reMatch(p, buf); err != nil {
 		return err
 	}
-	return fmt.Errorf("UnmarshalJSONPB unimplemented for %T", p)
+	i, err := strconv.ParseUint(string(buf), 10, 32)
+	if err != nil {
+		return fmt.Errorf("parse %s as 32-bit unsigned integer: %v", buf, err)
+	}
+	if i == 0 {
+		return errors.New("FHIR positiveInt primitive must be >0")
+	}
+	p.Value = uint32(i)
+	return nil
 }
 
+const noEmptyString = "FHIR string primitive can never be empty"
+
 // MarshalJSONPB implements the single method of the jsonpb.JSONPBMarshaler
-// interface.
+// interface, to properly handle this FHIR primitive type.
 func (p *String) MarshalJSONPB(_ *jsonpb.Marshaler) ([]byte, error) {
+	if p.Value == "" {
+		return nil, errors.New(noEmptyString)
+	}
 	return jsonString(p.Value), nil
 }
 
 // UnmarshalJSONPB implements the single method of the jsonpb.JSONPBUnmarshaler
-// interface.
+// interface, to properly handle this FHIR primitive type.
 func (p *String) UnmarshalJSONPB(_ *jsonpb.Unmarshaler, buf []byte) error {
-	if err := reMatch(p, buf); err != nil {
-		return err
-	}
-	s, err := parseJSONString(buf)
+	s, err := reMatchString(p, buf)
 	if err != nil {
 		return err
+	}
+	if s == "" {
+		return errors.New(noEmptyString)
 	}
 	p.Value = s
 	return nil
 }
 
 // MarshalJSONPB implements the single method of the jsonpb.JSONPBMarshaler
-// interface.
+// interface, to properly handle this FHIR primitive type.
 func (p *Time) MarshalJSONPB(_ *jsonpb.Marshaler) ([]byte, error) {
 	return nil, fmt.Errorf("MarshalJSONPB unimplemented for %T", p)
 }
 
 // UnmarshalJSONPB implements the single method of the jsonpb.JSONPBUnmarshaler
-// interface.
+// interface, to properly handle this FHIR primitive type.
 func (p *Time) UnmarshalJSONPB(_ *jsonpb.Unmarshaler, buf []byte) error {
 	if err := reMatch(p, buf); err != nil {
 		return err
@@ -334,28 +437,33 @@ func (p *Time) UnmarshalJSONPB(_ *jsonpb.Unmarshaler, buf []byte) error {
 }
 
 // MarshalJSONPB implements the single method of the jsonpb.JSONPBMarshaler
-// interface.
+// interface, to properly handle this FHIR primitive type.
 func (p *UnsignedInt) MarshalJSONPB(_ *jsonpb.Marshaler) ([]byte, error) {
-	return nil, fmt.Errorf("MarshalJSONPB unimplemented for %T", p)
+	return []byte(fmt.Sprintf("%d", p.Value)), nil
 }
 
 // UnmarshalJSONPB implements the single method of the jsonpb.JSONPBUnmarshaler
-// interface.
+// interface, to properly handle this FHIR primitive type.
 func (p *UnsignedInt) UnmarshalJSONPB(_ *jsonpb.Unmarshaler, buf []byte) error {
 	if err := reMatch(p, buf); err != nil {
 		return err
 	}
-	return fmt.Errorf("UnmarshalJSONPB unimplemented for %T", p)
+	i, err := strconv.ParseUint(string(buf), 10, 32)
+	if err != nil {
+		return fmt.Errorf("parse %s as 32-bit unsigned integer: %v", buf, err)
+	}
+	p.Value = uint32(i)
+	return nil
 }
 
 // MarshalJSONPB implements the single method of the jsonpb.JSONPBMarshaler
-// interface.
+// interface, to properly handle this FHIR primitive type.
 func (p *Uri) MarshalJSONPB(_ *jsonpb.Marshaler) ([]byte, error) {
 	return nil, fmt.Errorf("MarshalJSONPB unimplemented for %T", p)
 }
 
 // UnmarshalJSONPB implements the single method of the jsonpb.JSONPBUnmarshaler
-// interface.
+// interface, to properly handle this FHIR primitive type.
 func (p *Uri) UnmarshalJSONPB(_ *jsonpb.Unmarshaler, buf []byte) error {
 	if err := reMatch(p, buf); err != nil {
 		return err
@@ -364,13 +472,13 @@ func (p *Uri) UnmarshalJSONPB(_ *jsonpb.Unmarshaler, buf []byte) error {
 }
 
 // MarshalJSONPB implements the single method of the jsonpb.JSONPBMarshaler
-// interface.
+// interface, to properly handle this FHIR primitive type.
 func (p *Uuid) MarshalJSONPB(_ *jsonpb.Marshaler) ([]byte, error) {
 	return nil, fmt.Errorf("MarshalJSONPB unimplemented for %T", p)
 }
 
 // UnmarshalJSONPB implements the single method of the jsonpb.JSONPBUnmarshaler
-// interface.
+// interface, to properly handle this FHIR primitive type.
 func (p *Uuid) UnmarshalJSONPB(_ *jsonpb.Unmarshaler, buf []byte) error {
 	if err := reMatch(p, buf); err != nil {
 		return err
@@ -379,13 +487,13 @@ func (p *Uuid) UnmarshalJSONPB(_ *jsonpb.Unmarshaler, buf []byte) error {
 }
 
 // MarshalJSONPB implements the single method of the jsonpb.JSONPBMarshaler
-// interface.
+// interface, to properly handle this FHIR primitive type.
 func (p *Xhtml) MarshalJSONPB(_ *jsonpb.Marshaler) ([]byte, error) {
 	return nil, fmt.Errorf("MarshalJSONPB unimplemented for %T", p)
 }
 
 // UnmarshalJSONPB implements the single method of the jsonpb.JSONPBUnmarshaler
-// interface.
+// interface, to properly handle this FHIR primitive type.
 func (p *Xhtml) UnmarshalJSONPB(_ *jsonpb.Unmarshaler, buf []byte) error {
 	if err := reMatch(p, buf); err != nil {
 		return err
