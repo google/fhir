3abe3d3f3eb7d9e086e5b777cd1d81156683b895
Nick George <nickgeorge@google.com>
2019-01-09 12:37:22 -0800

    fixes typos
    PiperOrigin-RevId: 228567877

--- java/src/main/java/com/google/fhir/stu3/ResourceUtils.java
+++ java/src/main/java/com/google/fhir/stu3/ResourceUtils.java
@@ -10,7 +10,7 @@
 //    distributed under the License is distributed on an "AS IS" BASIS,
 //    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //    See the License for the specific language governing permissions and
-//    limitations under the License.package com.google.research.health.fhir.stu3;
+//    limitations under the License.
 
 package com.google.fhir.stu3;
 
