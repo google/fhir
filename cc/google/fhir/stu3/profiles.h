f852d215de2b57b4e042e01c869c5ac9463b81e9
Nick George <nickgeorge@google.com>
2019-03-05 21:14:48 -0800

    Adds comments to profiles.h
    PiperOrigin-RevId: 236977197

--- cc/google/fhir/stu3/profiles.h
+++ cc/google/fhir/stu3/profiles.h
@@ -26,12 +26,26 @@ namespace stu3 {
 
 using std::string;
 
+// Converts a resource to a profiled version of that resource.
+// If the profile adds new inlined fields for Extensions or Codings within
+// CodeableConcepts, and those extensions or codings are present on the base
+// message, the data will be reorganized into the new fields in the profile.
+// Finally, this runs the Fhir resource validation code on the resulting
+// message, to ensure the result complies with the requirements of the profile
+// (e.g., fields that are considered required by the profile).
 Status ConvertToProfile(const google::protobuf::Message& base_message,
                         google::protobuf::Message* profiled_message);
 
+// Identical to ConvertToProfile, except does not run the validation step.
+// TODO: delete this method, and instead use a different status
+// code in this case, that can be handled on the caller side.
 Status ConvertToProfileLenient(const google::protobuf::Message& base_message,
                                google::protobuf::Message* profiled_message);
 
+// Performs the inverse operation to ConvertToProfile.
+// Any data that is in an inlined field in the profiled message,
+// that does not exists in the base message will be converted back to the
+// original format (e.g., extension).
 Status ConvertToBaseResource(const google::protobuf::Message& profiled_message,
                              google::protobuf::Message* base_message);
 
