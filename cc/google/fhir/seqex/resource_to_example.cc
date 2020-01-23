9250a1edf0dec26d810fc6e6cac05b48f87c7249
Patrik Sundberg <sundberg@google.com>
2020-01-22 16:46:20 -0800

    Simplify handling of CodeableConcepts in ResourceToExample.
    PiperOrigin-RevId: 291059774

--- cc/google/fhir/seqex/resource_to_example.cc
+++ cc/google/fhir/seqex/resource_to_example.cc
@@ -29,10 +29,12 @@
 #include "absl/strings/str_replace.h"
 #include "absl/strings/str_split.h"
 #include "absl/time/time.h"
+#include "google/fhir/codeable_concepts.h"
+#include "google/fhir/codes.h"
+#include "google/fhir/fhir_types.h"
 #include "google/fhir/proto_util.h"
 #include "google/fhir/seqex/text_tokenizer.h"
 #include "google/fhir/status/status.h"
-#include "google/fhir/stu3/codeable_concepts.h"
 #include "google/fhir/util.h"
 #include "proto/annotations.pb.h"
 #include "proto/stu3/datatypes.pb.h"
@@ -97,30 +99,6 @@ static const std::vector<std::string>* const kIcd9Schemes =
     new std::vector<std::string>({kIcd9, kIcd9Diagnosis, kIcd9Procedure});
 static const std::vector<std::string>* const kIcd10Schemes =
     new std::vector<std::string>({kIcd10Diagnosis});
-
-// Format ICD9 Diagnosis code according to
-// http://www.icd9data.com/2015/Volume1/default.htm.
-// NOTE: Current implementation is fairly naive without validation / padding.
-std::string FormatIcd9Diagnosis(const std::string& icd9) {
-  if (icd9.length() <= 3 || (icd9[0] == 'E' && icd9.length() <= 4) ||
-      icd9.find('.') != std::string::npos) {
-    return icd9;
-  }
-  if (icd9[0] == 'E') {
-    return absl::StrCat(icd9.substr(0, 4), ".", icd9.substr(4, icd9.length()));
-  }
-  return absl::StrCat(icd9.substr(0, 3), ".", icd9.substr(3, icd9.length()));
-}
-
-// Format ICD9 Procedure code according to
-// http://www.icd9data.com/2012/Volume3/.
-// NOTE: Current implementation is fairly naive without validation / padding.
-std::string FormatIcd9Procedure(const std::string& icd9) {
-  if (icd9.length() <= 2 || icd9.find('.') != std::string::npos) {
-    return icd9;
-  }
-  return absl::StrCat(icd9.substr(0, 2), ".", icd9.substr(2, icd9.length()));
-}
 
 // Return a short form of the system name.
 std::string ToShortSystemName(const std::string& system) {
@@ -205,7 +183,6 @@ using ::google::fhir::stu3::proto::Time;
 using ::google::fhir::stu3::proto::Uri;
 using ::google::fhir::stu3::proto::Xhtml;
 using ::google::protobuf::Message;
-using ::tensorflow::Status;
 
 namespace {
 
@@ -260,114 +237,37 @@ void AddValueAndOrTokensToExample(
       ->add_value(value);
 }
 
-// Extract code value for a given system code. Return as soon as we find one.
-
-Status ExtractCodeBySystem(const Message& codeable_concept,
-                           absl::string_view system_value,
-                           std::string* result) {
-  auto status_or_result =
-      google::fhir::stu3::ExtractCodeBySystem(codeable_concept, system_value);
-  if (status_or_result.ok()) {
-    *result = status_or_result.ValueOrDie();
-    return Status::OK();
-  }
-  return status_or_result.status();
-}
-
-Status ExtractIcdCode(const Message& codeable_concept,
-                      const std::vector<std::string>& schemes,
-                      std::string* result) {
-  bool found_response = false;
-  std::string intermediate_result;
-  for (size_t i = 0; i < schemes.size(); i++) {
-    StatusOr<std::string> s =
-        google::fhir::stu3::ExtractCodeBySystem(codeable_concept, schemes[i]);
-
-    if (s.status().code() == ::tensorflow::errors::Code::ALREADY_EXISTS) {
-      // Multiple codes, so we can return an error already.
-      return s.status();
-    } else if (s.ok()) {
-      if (found_response) {
-        // We found _another_ code. That shouldn't have happened.
-        return ::tensorflow::errors::AlreadyExists("Found more than one code");
-      } else {
-        intermediate_result = s.ValueOrDie();
-        found_response = true;
-      }
-    }
-  }
-  if (found_response) {
-    *result = intermediate_result;
-    return Status::OK();
-  } else {
-    return ::tensorflow::errors::NotFound(
-        "No ICD code with the provided schemes in concept.");
-  }
-}
-
-absl::optional<std::string> GetCodeFromConceptText(const Message& concept) {
-  if (stu3::CodingSize(concept) > 0) {
-    return absl::nullopt;
-  }
-  const google::protobuf::FieldDescriptor* text_field =
-      concept.GetDescriptor()->FindFieldByName("text");
-  if (!text_field) {
-    return absl::nullopt;
-  }
-  if (!FieldHasValue(concept, text_field)) {
-    return absl::nullopt;
-  }
-  const auto& statusor_text =
-      GetMessageInField<stu3::proto::String>(concept, text_field);
-  if (!statusor_text.ok()) {
-    return absl::nullopt;
-  }
-  return absl::StrCat("text:" + statusor_text.ValueOrDie().value());
-}
-
 }  // namespace
 
-std::string GetCode(const Coding& coding) {
+void AddCodingToExample(const Coding& coding, const std::string& name,
+                        bool append_system_to_feature_name,
+                        ::tensorflow::Example* example,
+                        std::set<std::string>* tokenize_feature_set,
+                        const std::set<std::string>& add_tokenize_feature_set,
+                        const TextTokenizer& tokenizer,
+                        bool enable_attribution) {
+  CHECK(coding.has_system() && coding.has_code());
   const std::string system =
       systems::ToShortSystemName(coding.system().value());
-  return absl::StrCat(system, ":", coding.code().value());
-}
-
-void AddCodeableConceptToExample(
-    const Message& concept, const std::string& name,
-    ::tensorflow::Example* example, std::set<std::string>* tokenize_feature_set,
-    const std::set<std::string>& add_tokenize_feature_set,
-    const TextTokenizer& tokenizer, bool enable_attribution) {
-  const StatusOr<String>& statusor_text =
-      GetMessageInField<String>(concept, "text");
-  if (statusor_text.ok() && !statusor_text.ValueOrDie().value().empty()) {
-    const std::string full_name = absl::StrCat(name, ".text");
+  const std::string feature_name =
+      append_system_to_feature_name ? absl::StrCat(name, ".", system) : name;
+  const std::string code =
+      append_system_to_feature_name
+          ? coding.code().value()
+          : absl::StrCat(system, ":", coding.code().value());
+  (*example->mutable_features()->mutable_feature())[feature_name]
+      .mutable_bytes_list()
+      ->add_value(code);
+  if (coding.has_display()) {
+    const std::string full_name = absl::StrCat(feature_name, ".display");
     if (absl::GetFlag(FLAGS_tokenize_code_text_features)) {
       tokenize_feature_set->insert(full_name);
     }
     AddValueAndOrTokensToExample(
-        *tokenize_feature_set, add_tokenize_feature_set, tokenizer, full_name,
-        statusor_text.ValueOrDie().value(), example, enable_attribution);
+        *tokenize_feature_set, add_tokenize_feature_set, tokenizer,
+        absl::StrCat(name, ".", system, ".display"), coding.display().value(),
+        example, enable_attribution);
   }
-  stu3::ForEachCoding(concept, [&](const Coding& coding) {
-    CHECK(coding.has_system() && coding.has_code());
-    const std::string system =
-        systems::ToShortSystemName(coding.system().value());
-    (*example->mutable_features()
-          ->mutable_feature())[absl::StrCat(name, ".", system)]
-        .mutable_bytes_list()
-        ->add_value(coding.code().value());
-    if (coding.has_display()) {
-      const std::string full_name = absl::StrCat(name, ".", system, ".display");
-      if (absl::GetFlag(FLAGS_tokenize_code_text_features)) {
-        tokenize_feature_set->insert(full_name);
-      }
-      AddValueAndOrTokensToExample(
-          *tokenize_feature_set, add_tokenize_feature_set, tokenizer,
-          absl::StrCat(name, ".", system, ".display"), coding.display().value(),
-          example, enable_attribution);
-    }
-  });
 }
 
 void MessageToExample(const google::protobuf::Message& message, const std::string& prefix,
@@ -413,42 +313,13 @@ void MessageToExample(const google::protobuf::Message& message, const std::strin
                 : reflection->GetMessage(message, field);
 
         // Handle some fhir types explicitly, including primitive STU3 types.
-        if (field->message_type()->full_name() ==
-            // Codes are emitted as-is, without tokenization.
-            stu3::proto::Code::descriptor()->full_name()) {
+        if (IsTypeOrProfileOfCode(child)) {
+          // Codes are emitted as-is, without tokenization.
           stu3::proto::Code code;
-          code.CopyFrom(child);
+          CHECK(ConvertToGenericCode(child, &code).ok());
           (*example->mutable_features()->mutable_feature())[name]
               .mutable_bytes_list()
               ->add_value(code.value());
-        } else if (field->message_type()->options().HasExtension(
-                       proto::fhir_valueset_url)) {
-          // Valueset-constrained codes are emitted without tokenization.
-          // Note: some codes (i.e. MimeTypeCode) are represented as non
-          // enumerated strings - handled here.
-          const google::protobuf::Reflection* reflection = child.GetReflection();
-          const google::protobuf::FieldDescriptor* value_field =
-              field->message_type()->FindFieldByName("value");
-          auto* bytes_list =
-              (*example->mutable_features()->mutable_feature())[name]
-                  .mutable_bytes_list();
-          switch (value_field->cpp_type()) {
-            case google::protobuf::FieldDescriptor::CPPTYPE_ENUM: {
-              const google::protobuf::EnumValueDescriptor* enum_value =
-                  reflection->GetEnum(child, value_field);
-              bytes_list->add_value(absl::AsciiStrToLower(enum_value->name()));
-              break;
-            }
-            case google::protobuf::FieldDescriptor::CPPTYPE_STRING: {
-              std::string str;
-              str = reflection->GetStringReference(child, value_field, &str);
-              bytes_list->add_value(absl::AsciiStrToLower(str));
-              break;
-            }
-            default:
-              LOG(FATAL)
-                  << "Unrecognized code type found in resource_to_example.";
-          }
         } else if (field->message_type()->full_name() ==
                    Date::descriptor()->full_name()) {
           Date date;
@@ -485,9 +356,15 @@ void MessageToExample(const google::protobuf::Message& message, const std::strin
                    String::descriptor()->full_name()) {
           String s;
           s.CopyFrom(child);
-          AddValueAndOrTokensToExample(
-              tokenize_feature_set, add_tokenize_feature_set, tokenizer, name,
-              s.value(), example, enable_attribution);
+          if (absl::GetFlag(FLAGS_tokenize_code_text_features) &&
+              field->name() == "text" && IsCodeableConceptLike(message)) {
+            AddTokensToExample(name, s.value(), tokenizer, example,
+                               enable_attribution);
+          } else {
+            AddValueAndOrTokensToExample(
+                tokenize_feature_set, add_tokenize_feature_set, tokenizer, name,
+                s.value(), example, enable_attribution);
+          }
         } else if (field->message_type()->full_name() ==
                    Xhtml::descriptor()->full_name()) {
           Xhtml xhtml;
@@ -525,20 +402,35 @@ void MessageToExample(const google::protobuf::Message& message, const std::strin
           (*example->mutable_features()->mutable_feature())[name]
               .mutable_float_list()
               ->add_value(value_as_double);
-        } else if (stu3::IsCodeableConceptLike(field->message_type())) {
-          // Codeable concepts are emitted using the preferred coding systems.
-          AddCodeableConceptToExample(
-              child, name, example, &tokenize_feature_set,
-              add_tokenize_feature_set, tokenizer, enable_attribution);
-        } else if (field->message_type()->full_name() ==
-                   Coding::descriptor()->full_name()) {
+        } else if (IsTypeOrProfileOfCoding(field->message_type())) {
           // Codings are emitted with the system if we know it, as raw codes
           // otherwise.
+          bool use_name_in_feature_name = IsCodeableConceptLike(message);
           Coding coding;
-          coding.CopyFrom(child);
-          (*example->mutable_features()->mutable_feature())[name]
-              .mutable_bytes_list()
-              ->add_value(GetCode(coding));
+          auto status = ConvertToGenericCoding(child, &coding);
+          if (!status.ok() &&
+              field->message_type()->full_name() ==
+                  stu3::proto::CodingWithFixedSystem::descriptor()
+                      ->full_name()) {
+            // TODO: remove this code path.
+            stu3::proto::CodingWithFixedSystem fixed_coding;
+            fixed_coding.CopyFrom(child);
+            coding.mutable_code()->CopyFrom(fixed_coding.code());
+            *coding.mutable_system()->mutable_value() =
+                GetInlinedCodingSystem(field);
+            if (fixed_coding.has_display()) {
+              coding.mutable_display()->CopyFrom(fixed_coding.display());
+            }
+            AddCodingToExample(coding, prefix, true, example,
+                               &tokenize_feature_set, add_tokenize_feature_set,
+                               tokenizer, enable_attribution);
+          } else {
+            CHECK(status.ok()) << status;
+            AddCodingToExample(coding, use_name_in_feature_name ? prefix : name,
+                               use_name_in_feature_name, example,
+                               &tokenize_feature_set, add_tokenize_feature_set,
+                               tokenizer, enable_attribution);
+          }
         } else if (field->message_type()->full_name() ==
                    Extension::descriptor()->full_name()) {
           // We do not emit extensions. To include data encoded in extensions
@@ -567,8 +459,9 @@ void MessageToExample(const google::protobuf::Message& message, const std::strin
           MessageToExample(child, name, tokenizer, example, enable_attribution);
         }
       } else {
-        LOG(INFO) << "Unable to handle field " << name << " in message of type "
-                  << message.GetDescriptor()->full_name();
+        LOG(ERROR) << "Unable to handle field " << name
+                   << " in message of type "
+                   << message.GetDescriptor()->full_name();
       }
     }
   }
