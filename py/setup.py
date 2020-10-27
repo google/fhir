2b8b016520fce6234d348cf56e0c8bbd8793b7b4
Cameron Tew <ctew@google.com>
2020-10-27 18:55:39 +0000

    Bump pypi package version to 0.6.2.
    PiperOrigin-RevId: 339297557

--- py/setup.py
+++ py/setup.py
@@ -115,7 +115,7 @@ long_description = pathlib.Path(_HERE).joinpath('README.md').read_text()
 
 setuptools.setup(
     name='google-fhir',
-    version='0.6.1',
+    version='0.6.2',
     description='Tools for parsing and printing FHIR JSON.',
     long_description=long_description,
     long_description_content_type='text/markdown',
