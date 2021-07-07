793587796dabbe4ba932af2aeeb1dc61ff130f36
Nick George <nickgeorge@google.com>
2021-07-07 19:22:55 +0000

    Adds java utility for retagging newly generated messages based on a reference version of the message.  This will be wired into the ProtoGenerator to guarantee stable tag numbers
    PiperOrigin-RevId: 383467749

---
+++ java/com/google/fhir/protogen/FieldRetagger.java
@@ -0,0 +1,157 @@
+//    Copyright 2021 Google Inc.
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        https://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
+package com.google.fhir.protogen;
+
+import static java.lang.Math.max;
+import static java.util.stream.Collectors.toMap;
+
+import com.google.fhir.proto.ProtoGeneratorAnnotations;
+import com.google.protobuf.DescriptorProtos.DescriptorProto;
+import com.google.protobuf.DescriptorProtos.DescriptorProtoOrBuilder;
+import com.google.protobuf.DescriptorProtos.FieldDescriptorProto;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Utility for comparing new versions of a proto to old versions of the proto, to ensure that no tag
+ * numbers are altered.
+ */
+final class FieldRetagger {
+
+  private FieldRetagger() {}
+
+  /**
+   * Given a new message and a golden message, returns a copy of the new message that guarantees
+   * that, for that message and all submessages contained within both the new and golden message:
+   *
+   * <ol>
+   *   <li>If a field within the message exists in both new and golden, they will have the same tag
+   *       number
+   *   <li>If a field exists with the new but not the golden, it will have a tag number greater than
+   *       any in the golden, or than the last reserved field in the golden.
+   *   <li>If the highest-numbered field from the golden has been removed and no new fields added,
+   *       adds a reserved field with the same number as the removed field, to guarantee that that
+   *       number will never be reused. Note that this does not insert reserved ranges for
+   *       lower-numbered removed fields, since all new fields are added with numbers greater than
+   *       any in the golden, so there is no risk of reusing a lower number.
+   * </ol>
+   *
+   * Note that this does not sort fields by tag numbers, so the tag numbers may no longer be
+   * consecutive. This causes the fields to appear in the printed proto in the same order they are
+   * defined in the FHIR Profile, regardless of any tag adjustments needed.
+   */
+  static DescriptorProto retagMessage(DescriptorProto newMessage, DescriptorProto golden) {
+    DescriptorProto.Builder newBuilder = newMessage.toBuilder();
+    retagMessage(newBuilder, golden);
+    return newBuilder.build();
+  }
+
+  private static void retagMessage(DescriptorProto.Builder newBuilder, DescriptorProto golden) {
+    Map<String, FieldDescriptorProto> referenceMap = getFieldMap(golden.getFieldList());
+    // List of fields with no corresponding field in the golden message.
+    // These should be assigned numbers greater than any in use in the golden message.
+    List<FieldDescriptorProto.Builder> newFields = new ArrayList<>();
+
+    for (FieldDescriptorProto.Builder fieldBuilder : newBuilder.getFieldBuilderList()) {
+      FieldDescriptorProto goldenField = referenceMap.get(fieldBuilder.getName());
+      if (goldenField == null) {
+        if (fieldBuilder.getOptions().hasExtension(ProtoGeneratorAnnotations.reservedReason)) {
+          checkReservedField(fieldBuilder.getNumber(), golden);
+        } else {
+          newFields.add(fieldBuilder);
+        }
+      } else {
+        if (goldenField.getNumber() != fieldBuilder.getNumber()) {
+          fieldBuilder.setNumber(goldenField.getNumber());
+        }
+      }
+    }
+
+    int highestGoldenTag = getHighestNumberInUse(golden);
+
+    if (!newFields.isEmpty()) {
+      // Assign any new fields to numbers greater than any on the golden message.
+      // Assuming both builder file and golden file were not malformed to begin with, this will
+      // guarantee that no field is reused and all corresponding fields have identical numbers.
+      int nextTag = highestGoldenTag + 1;
+      for (FieldDescriptorProto.Builder newField : newFields) {
+        newField.setNumber(nextTag++);
+      }
+    }
+
+    // If the golden has a higher tag number (either present or reserved) than the highest tag
+    // in the new file, add a reserved field for the highest golden tag.
+    // This ensures that all subsequent iterations of the proto will never use that or any
+    // lower-numbered tag.
+    // TODO: For historical reasons, ProtoGenerator generates reserved fields using
+    // normal fields, with the "reservedReason" annotation.  These should be updated to just use
+    // ReservedRanges.
+    if (getHighestNumberInUse(newBuilder) < highestGoldenTag) {
+      newBuilder
+          .addFieldBuilder()
+          .setNumber(highestGoldenTag)
+          .getOptionsBuilder()
+          .setExtension(
+              ProtoGeneratorAnnotations.reservedReason,
+              "Field "
+                  + highestGoldenTag
+                  + " reserved to prevent reuse of field that was previously deleted.");
+    }
+
+    retagMessages(newBuilder.getNestedTypeBuilderList(), golden.getNestedTypeList());
+  }
+
+  private static void retagMessages(
+      List<DescriptorProto.Builder> builderMessages, List<DescriptorProto> goldenMessages) {
+    Map<String, DescriptorProto> referenceMap = getMessageMap(goldenMessages);
+    for (DescriptorProto.Builder messageBuilder : builderMessages) {
+      DescriptorProto goldenMessage = referenceMap.get(messageBuilder.getName());
+      if (goldenMessage != null) {
+        retagMessage(messageBuilder, goldenMessage);
+      }
+    }
+  }
+
+  private static int getHighestNumberInUse(DescriptorProtoOrBuilder message) {
+    int highestTagNumber =
+        message.getFieldList().stream().map(FieldDescriptorProto::getNumber).reduce(0, Math::max);
+    int highestReserved =
+        message.getReservedRangeList().stream()
+            .map(range -> range.getEnd() - 1 /* end is exclusive */)
+            .reduce(0, Math::max);
+    return max(highestTagNumber, highestReserved);
+  }
+
+  private static Map<String, DescriptorProto> getMessageMap(List<DescriptorProto> messages) {
+    return messages.stream().collect(toMap(DescriptorProto::getName, message -> message));
+  }
+
+  private static Map<String, FieldDescriptorProto> getFieldMap(List<FieldDescriptorProto> fields) {
+    return fields.stream().collect(toMap(FieldDescriptorProto::getName, field -> field));
+  }
+
+  private static void checkReservedField(int number, DescriptorProto golden) {
+    for (DescriptorProto.ReservedRange range : golden.getReservedRangeList()) {
+      if (number >= range.getStart() && number < range.getEnd()) {
+        return;
+      }
+    }
+
+    throw new IllegalStateException(
+        "Encountered unexpected reserved field in new proto that does not exist in reference"
+            + " proto.");
+  }
+}
---
+++ javatests/com/google/fhir/protogen/FieldRetaggerTest.java
@@ -0,0 +1,266 @@
+//    Copyright 2021 Google Inc.
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        https://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
+package com.google.fhir.protogen;
+
+import static com.google.common.truth.extensions.proto.ProtoTruth.assertThat;
+import static org.junit.Assert.assertThrows;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.fhir.proto.ProtoGeneratorAnnotations;
+import com.google.protobuf.DescriptorProtos.DescriptorProto;
+import java.util.Map;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public final class FieldRetaggerTest {
+
+  private DescriptorProto.Builder makeDescriptor(
+      String messageName, Map<String, Integer> fieldMap) {
+    DescriptorProto.Builder builder = DescriptorProto.newBuilder().setName(messageName);
+    fieldMap.forEach((name, number) -> builder.addFieldBuilder().setName(name).setNumber(number));
+    return builder;
+  }
+
+  // TODO: For historical reasons, ProtoGenerator generates reserved fields using
+  // normal fields, with the "reservedReason" annotation.  These should be updated to just use
+  // ReservedRanges.
+  private DescriptorProto.Builder addReservedField(DescriptorProto.Builder builder, int number) {
+    builder
+        .addFieldBuilder()
+        .setNumber(number)
+        .getOptionsBuilder()
+        .setExtension(
+            ProtoGeneratorAnnotations.reservedReason,
+            "Field " + number + " reserved to prevent reuse of field that was previously deleted.");
+    return builder;
+  }
+
+  private DescriptorProto.Builder addReservedFieldViaRange(
+      DescriptorProto.Builder builder, int number) {
+    builder.addReservedRangeBuilder().setStart(number).setEnd(number + 1);
+    return builder;
+  }
+
+  @Test
+  public void retagMessage_identicalMessages() {
+    DescriptorProto newMessage =
+        makeDescriptor("m1", ImmutableMap.of("m1f1", 1, "m1f2", 2, "m1f3", 3))
+            .addNestedType(makeDescriptor("m1s1", ImmutableMap.of("s1f1", 1, "s1f2", 2, "s1f3", 3)))
+            .build();
+
+    // Note that in goldens, the order of fields is different than that in newFile.  This should
+    // have no effect.
+
+    DescriptorProto goldenMessage =
+        makeDescriptor("m1", ImmutableMap.of("m1f1", 1, "m1f3", 3, "m1f2", 2))
+            .addNestedType(makeDescriptor("m1s1", ImmutableMap.of("s1f2", 2, "s1f3", 3, "s1f1", 1)))
+            .build();
+
+    DescriptorProto expectedMessage = newMessage.toBuilder().build();
+
+    assertThat(FieldRetagger.retagMessage(newMessage, goldenMessage)).isEqualTo(expectedMessage);
+  }
+
+  @Test
+  public void retagMessage_noMismatch_newFieldsAtEnd() {
+    DescriptorProto newMessage =
+        makeDescriptor("m1", ImmutableMap.of("m1f1", 1, "m1f2", 2, "m1f3", 3))
+            .addNestedType(makeDescriptor("m1s1", ImmutableMap.of("s1f1", 1, "s1f2", 2, "s1f3", 3)))
+            .build();
+
+    DescriptorProto goldenMessage =
+        makeDescriptor("m1", ImmutableMap.of("m1f1", 1, "m1f2", 2))
+            .addNestedType(makeDescriptor("m1s1", ImmutableMap.of("s1f1", 1, "s1f2", 2)))
+            .build();
+
+    DescriptorProto expectedMessage = newMessage.toBuilder().build();
+
+    assertThat(FieldRetagger.retagMessage(newMessage, goldenMessage)).isEqualTo(expectedMessage);
+  }
+
+  @Test
+  public void retagMessage_noMismatch_newFieldsNotAtEndMovedToEnd() {
+    // In these examples, the "f5" and "f6" fields have tag numbers 2 and 3, but no corresponding
+    // field in the goldens.  These should be moved tag numbers greater than any in the golden
+    // message. The fields are not reordered.
+
+    DescriptorProto newMessage =
+        makeDescriptor("m1", ImmutableMap.of("m1f1", 1, "m1f5", 2, "m1f6", 3, "m1f4", 4))
+            .addNestedType(
+                makeDescriptor("m1s1", ImmutableMap.of("s1f1", 1, "s1f5", 2, "s1f6", 3, "s1f4", 4)))
+            .build();
+
+    DescriptorProto goldenMessage =
+        makeDescriptor("m1", ImmutableMap.of("m1f1", 1, "m1f4", 4))
+            .addNestedType(makeDescriptor("m1s1", ImmutableMap.of("s1f1", 1, "s1f4", 4)))
+            .build();
+
+    DescriptorProto expectedMessage =
+        makeDescriptor("m1", ImmutableMap.of("m1f1", 1, "m1f5", 5, "m1f6", 6, "m1f4", 4))
+            .addNestedType(
+                makeDescriptor("m1s1", ImmutableMap.of("s1f1", 1, "s1f5", 5, "s1f6", 6, "s1f4", 4)))
+            .build();
+
+    assertThat(FieldRetagger.retagMessage(newMessage, goldenMessage)).isEqualTo(expectedMessage);
+  }
+
+  @Test
+  public void retagMessage_mismatchFound_noNewFields() {
+    // In this example, the "f2" and "f3" fields have been reversed in order and tag number.
+    // They should keep the ordering of the new message, but renumber to use the field numbers of
+    // the golden.
+
+    DescriptorProto newMessage =
+        makeDescriptor("m1", ImmutableMap.of("m1f1", 1, "m1f3", 2, "m1f2", 3))
+            .addNestedType(makeDescriptor("m1s1", ImmutableMap.of("s1f1", 1, "s1f3", 2, "s1f2", 3)))
+            .build();
+
+    DescriptorProto goldenMessage =
+        makeDescriptor("m1", ImmutableMap.of("m1f1", 1, "m1f2", 2, "m1f3", 3))
+            .addNestedType(makeDescriptor("m1s1", ImmutableMap.of("s1f1", 1, "s1f2", 2, "s1f3", 3)))
+            .build();
+
+    DescriptorProto expectedMessage =
+        makeDescriptor("m1", ImmutableMap.of("m1f1", 1, "m1f3", 3, "m1f2", 2))
+            .addNestedType(makeDescriptor("m1s1", ImmutableMap.of("s1f1", 1, "s1f3", 3, "s1f2", 2)))
+            .build();
+
+    assertThat(FieldRetagger.retagMessage(newMessage, goldenMessage)).isEqualTo(expectedMessage);
+  }
+
+  @Test
+  public void retagMessage_mismatchFound_newFields() {
+    // In this example, the "f5" has been added to the new message using a tag number (3) that is
+    // not in use, but is not greater than any already in use, and "f2" and "f4" fields have
+    // incorrect tag numbers. The ordering of fields should be unchanged, but "f2" and "f4" should
+    // use the same tag numbers as in the golden, and "f5" will be assigned a new tag number.
+
+    DescriptorProto newMessage =
+        makeDescriptor("m1", ImmutableMap.of("m1f1", 1, "m1f4", 2, "m1f5", 3, "m1f2", 4))
+            .addNestedType(
+                makeDescriptor("m1s1", ImmutableMap.of("s1f1", 1, "s1f4", 2, "s1f5", 3, "s1f2", 4)))
+            .build();
+
+    DescriptorProto goldenMessage =
+        makeDescriptor("m1", ImmutableMap.of("m1f1", 1, "m1f2", 2, "m1f4", 4))
+            .addNestedType(makeDescriptor("m1s1", ImmutableMap.of("s1f1", 1, "s1f2", 2, "s1f4", 4)))
+            .build();
+
+    DescriptorProto expectedMessage =
+        makeDescriptor("m1", ImmutableMap.of("m1f1", 1, "m1f4", 4, "m1f5", 5, "m1f2", 2))
+            .addNestedType(
+                makeDescriptor("m1s1", ImmutableMap.of("s1f1", 1, "s1f4", 4, "s1f5", 5, "s1f2", 2)))
+            .build();
+
+    assertThat(FieldRetagger.retagMessage(newMessage, goldenMessage)).isEqualTo(expectedMessage);
+  }
+
+  @Test
+  public void retagMessage_lastFieldDeleted_reservedFieldAdded() {
+    // In this example, the "f1" and "f3" fields have been removed.  Since "f3" fields are the
+    // highest-numbered fields present, they should be replaced with a reserved number.
+
+    DescriptorProto newMessage =
+        makeDescriptor("m1", ImmutableMap.of("m1f2", 2))
+            .addNestedType(makeDescriptor("m1s1", ImmutableMap.of("s1f2", 2)))
+            .build();
+
+    DescriptorProto goldenMessage =
+        makeDescriptor("m1", ImmutableMap.of("m1f1", 1, "m1f2", 2, "m1f3", 3))
+            .addNestedType(makeDescriptor("m1s1", ImmutableMap.of("s1f1", 1, "s1f2", 2, "s1f3", 3)))
+            .build();
+
+    DescriptorProto expectedMessage =
+        addReservedField(makeDescriptor("m1", ImmutableMap.of("m1f2", 2)), 3)
+            .addNestedType(addReservedField(makeDescriptor("m1s1", ImmutableMap.of("s1f2", 2)), 3))
+            .build();
+
+    assertThat(FieldRetagger.retagMessage(newMessage, goldenMessage)).isEqualTo(expectedMessage);
+  }
+
+  @Test
+  public void retagMessage_noChanges_reservedFieldPreserved() {
+    // In this example, no field changes occur, but the golden contains a high reserved range.
+    // That should be persisted in the new file.
+
+    DescriptorProto newMessage =
+        makeDescriptor("m1", ImmutableMap.of("m1f2", 2))
+            .addNestedType(makeDescriptor("m1s1", ImmutableMap.of("s1f2", 2)))
+            .build();
+
+    DescriptorProto goldenMessage =
+        addReservedFieldViaRange(makeDescriptor("m1", ImmutableMap.of("m1f2", 2)), 3)
+            .addNestedType(
+                addReservedFieldViaRange(makeDescriptor("m1s1", ImmutableMap.of("s1f2", 2)), 3))
+            .build();
+
+    DescriptorProto expectedMessage =
+        addReservedField(makeDescriptor("m1", ImmutableMap.of("m1f2", 2)), 3)
+            .addNestedType(addReservedField(makeDescriptor("m1s1", ImmutableMap.of("s1f2", 2)), 3))
+            .build();
+
+    assertThat(FieldRetagger.retagMessage(newMessage, goldenMessage)).isEqualTo(expectedMessage);
+  }
+
+  @Test
+  public void retagMessage_newFieldAndReservedRange_fieldAddedAboveReservedRange() {
+    // In this example, no field changes occur, but the golden contains a high reserved range.
+    // That should be persisted in the new file.
+
+    DescriptorProto newMessage =
+        makeDescriptor("m1", ImmutableMap.of("m1f2", 2, "m1f4", 3))
+            .addNestedType(makeDescriptor("m1s1", ImmutableMap.of("s1f2", 2, "s1f4", 3)))
+            .build();
+
+    DescriptorProto goldenMessage =
+        addReservedFieldViaRange(makeDescriptor("m1", ImmutableMap.of("m1f2", 2)), 3)
+            .addNestedType(
+                addReservedFieldViaRange(makeDescriptor("m1s1", ImmutableMap.of("s1f2", 2)), 3))
+            .build();
+
+    DescriptorProto expectedMessage =
+        makeDescriptor("m1", ImmutableMap.of("m1f2", 2, "m1f4", 4))
+            .addNestedType(makeDescriptor("m1s1", ImmutableMap.of("s1f2", 2, "s1f4", 4)))
+            .build();
+
+    assertThat(FieldRetagger.retagMessage(newMessage, goldenMessage)).isEqualTo(expectedMessage);
+  }
+
+  @Test
+  public void retagMessage_matchingReservedField_preservedInResult() {
+    DescriptorProto newMessage =
+        addReservedField(makeDescriptor("m1", ImmutableMap.of("m1f2", 2, "m1f4", 3)), 1).build();
+
+    DescriptorProto goldenMessage =
+        addReservedFieldViaRange(makeDescriptor("m1", ImmutableMap.of("m1f2", 2)), 1).build();
+
+    DescriptorProto expectedMessage = newMessage.toBuilder().build();
+
+    assertThat(FieldRetagger.retagMessage(newMessage, goldenMessage)).isEqualTo(expectedMessage);
+  }
+
+  @Test
+  public void retagMessage_mismatchingReserved_throwsException() {
+    DescriptorProto newMessage =
+        addReservedField(makeDescriptor("m1", ImmutableMap.of("m1f2", 2, "m1f4", 3)), 1).build();
+
+    DescriptorProto goldenMessage = makeDescriptor("m1", ImmutableMap.of("m1f2", 2)).build();
+
+    assertThrows(
+        IllegalStateException.class, () -> FieldRetagger.retagMessage(newMessage, goldenMessage));
+  }
+}
