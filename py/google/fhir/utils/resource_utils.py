e3ccb0f68ef37e0f54e4352b1809fa43a5ec00c0
Cameron Tew <ctew@google.com>
2021-07-19 23:06:22 +0000

    Add utility module for working with FHIR resources.
    PiperOrigin-RevId: 385661811

---
+++ py/google/fhir/utils/resource_utils.py
@@ -0,0 +1,54 @@
+#
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""A collection of functions for dealing with FHIR resources."""
+
+from typing import cast, Any, List, Type, TypeVar
+
+from google.protobuf import message
+from google.fhir.utils import fhir_types
+from google.fhir.utils import path_utils
+
+_T = TypeVar('_T', bound=message.Message)
+
+
+def extract_resources_from_bundle(bundle: message.Message, *,
+                                  resource_type: Type[_T]) -> List[_T]:
+  """Returns a list of resources of type `resource_type` from `bundle`.
+
+  Args:
+    bundle: The FHIR Bundle to examine.
+    resource_type: The message type of the resource to return.
+
+  Returns:
+    A list of resources of type `resource_type` belonging to the bundle.
+
+  Raises:
+    TypeError: In the event that `bundle` is not of type "Bundle".
+    ValueError: In the event that a field corresponding to the "snake_case" name
+    of `resource_type` does not exist on `Bundle.Entry`.
+  """
+  # TODO: Replace with templated/consolidated `fhir_types` call.
+  if not fhir_types.is_type_or_profile_of(
+      'http://hl7.org/fhir/StructureDefinition/Bundle', bundle):
+    raise TypeError(
+        f'{bundle.DESCRIPTOR.name} is not a type or profile of Bundle.')
+
+  contained_resource_field = path_utils.camel_case_to_snake_case(
+      resource_type.DESCRIPTOR.name)
+  return [
+      getattr(entry.resource, contained_resource_field)
+      for entry in cast(Any, bundle).entry
+      if entry.resource.HasField(contained_resource_field)
+  ]
