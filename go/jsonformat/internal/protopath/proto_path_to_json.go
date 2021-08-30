5ac6d4e0e3af5c058b58ba576489bb7bbf6d0b3d
Googler <noreply@google.com>
2021-08-30 14:29:09 +0000

    Add protopath ToJSONPath func that converts a protopath and proto to the corresponding JSON path.
    PiperOrigin-RevId: 393767155

---
+++ go/jsonformat/internal/protopath/proto_path_to_json.go
@@ -0,0 +1,69 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package protopath
+
+import (
+	"strings"
+
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/reflect/protoreflect"
+)
+
+// ToJSONPath converts the `path` corresponding to the `proto` in to the JSON
+// path, providing a cleaner way to return paths to users for debugging. Therefore,
+// errors are ignored and conversion is simply aborted early.
+func ToJSONPath(p proto.Message, path Path) string {
+	if !isValidPath(path) {
+		return ""
+	}
+	return strings.Join(toJSON(p.ProtoReflect().Descriptor(), path.parts), ".")
+}
+
+func toJSON(md protoreflect.MessageDescriptor, path []protoreflect.Name) []string {
+	var jsonParts []string
+	if len(path) == 0 {
+		return jsonParts
+	}
+
+	name := path[0]
+	path = path[1:]
+
+	// Find the valid FieldDescriptor by name.
+	fd := md.Fields().ByName(name)
+	if fd == nil {
+		od := md.Oneofs().ByName(name)
+		// If this is a oneOf, we move one down into the oneOf as a oneOf doesn't
+		// have a MessageDescriptor to pass on the next call.
+		if od != nil && len(path) >= 1 {
+			fd = od.Fields().ByName(path[0])
+			path = path[1:]
+		}
+		if fd == nil {
+			return jsonParts
+		}
+	}
+
+	// Extract the JSON representation of the FieldDescriptor.
+	jsonName := fd.JSONName()
+	if jsonName != "" && fd.Cardinality() == protoreflect.Repeated && len(path) >= 1 {
+		jsonName += "[" + string(path[0]) + "]"
+		path = path[1:]
+	}
+
+	if jsonName != "" {
+		jsonParts = append(jsonParts, jsonName)
+	}
+	return append(jsonParts, toJSON(fd.Message(), path)...)
+}
---
+++ go/jsonformat/internal/protopath/proto_path_to_json_test.go
@@ -0,0 +1,84 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package protopath
+
+import (
+	"testing"
+
+	pptpb "github.com/google/fhir/go/jsonformat/internal/protopath/protopathtest_go_proto"
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestToJSONPath(t *testing.T) {
+	tests := []struct {
+		name string
+		path Path
+		want string
+	}{
+		{
+			"single path",
+			NewPath("string_field"),
+			"stringField",
+		},
+		{
+			"nested path",
+			NewPath("message_field.inner_field"),
+			"messageField.innerField",
+		},
+		{
+			"repeated inner path",
+			NewPath("repeated_message_field.0.inner_field"),
+			"repeatedMessageField[0].innerField",
+		},
+		{
+			"repeated leaf path",
+			NewPath("repeated_message_field.0"),
+			"repeatedMessageField[0]",
+		},
+		{
+			"oneof prefix path",
+			NewPath("oneof.oneof_message_field.inner_field"),
+			"oneofMessageField.innerField",
+		},
+		{
+			"oneof path",
+			NewPath("oneof_message_field.inner_field"),
+			"oneofMessageField.innerField",
+		},
+		{
+			"nonexistent path",
+			NewPath("message_field.invalid"),
+			"messageField",
+		},
+		{
+			"invalid path",
+			NewPath("foo."),
+			"",
+		},
+		{
+			"json annotated",
+			NewPath("json_annotated_string_field"),
+			"jsonAnnotated",
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			json := ToJSONPath(&pptpb.Message{}, test.path)
+			if diff := cmp.Diff(json, test.want); diff != "" {
+				t.Errorf("ToJSONPath(_, %v) => %v, want %v, diff:\n%s", test.path, json, test.want, diff)
+			}
+		})
+	}
+}
