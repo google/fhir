af14d17e52fd412d1ecd3a7ed856c7ae772e92a2
Suyash Kumar <suyashk@google.com>
2023-03-09 13:20:42 -0800

    Update generate_go_protos with some automatic setup, if needed.
    PiperOrigin-RevId: 515423150

--- go/generate_go_protos_default.sh
+++ go/generate_go_protos_default.sh
@@ -13,11 +13,34 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# In order to run this command, you must have protoc and the protoc-gen-go
-# plugin installed in your PATH.
-# More details on protoc-gen-go install: https://protobuf.dev/reference/go/go-generated/
-# Install protoc on linux with sudo apt install -y protobuf-compiler.
+# In order to run this script, you must have Go installed on your machine. This
+# script will attempt to setup a GOPATH and GOBIN in your $HOME directory, if
+# those environment variables are not already set. It will then attempt to
+# install the dependencies needed to run (protoc-gen-go and protoc, needed to
+# compile the protos).
 
 # This should be run from the root FHIR repository, like so:
 # ./go/generate_go_protos_default.sh
+
+if [ "${GOPATH}" = "" ]; then
+  echo "Setting temp GOPATH environment variable, creating GOPATH directory \$HOME/go"
+  GOPATH="${HOME}/go"
+  mkdir -p $GOPATH
+fi
+
+if [ "${GOBIN}" = "" ]; then
+  echo "Setting temp GOBIN environment variable, creating GOBIN directory in \$GOPATH/bin"
+  GOBIN="${GOPATH}/bin"
+  mkdir -p $GOBIN
+fi
+
+if ! [ -x "$(command -v protoc)" ]; then
+  echo 'protoc missing, please install the protobuf compiler. On linux: sudo apt install -y protobuf-compiler'
+fi
+
+if ! [ -x "$(command -v protoc-gen-go)" ]; then
+  go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.29
+  PATH=$PATH:$GOBIN
+fi
+
 bazel run //go:generate_go_protos -- --repo-path=.
